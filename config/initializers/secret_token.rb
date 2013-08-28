4d62dd8bfd75891a066034ee2ca9dbf90ad57542
Eric Levine <djlevine@gmail.com>
2013-08-28 18:13:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WagerWar::Application.config.secret_token = 'e01387a6f629ebd0580310e794235935f3f035bfd0e51c31ecc52baf54be663ed78a4dc1d92bf28574511976e95ce759a729bb44acd4cacdaa25b464d45b2539'

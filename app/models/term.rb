96770946d2edd3372e08f1d213a545523d58354b
Eric Levine <djlevine@gmail.com>
2013-08-29 16:26:35 -0500

    Added terms model

---
+++ app/models/term.rb
@@ -0,0 +1,4 @@
+class Term < ActiveRecord::Base
+  belongs_to :wager
+  # attr_accessible :title, :body
+end
--- app/models/wager.rb
+++ app/models/wager.rb
@@ -2,6 +2,7 @@ class Wager < ActiveRecord::Base
   has_and_belongs_to_many :users
   belongs_to :host,  class_name: "User", foreign_key: "host_id"
   belongs_to :guest,  class_name: "User", foreign_key: "guest_id"  
-  
+  has_many :terms
+
   # attr_accessible :title, :body
 end

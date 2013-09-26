e9559f70c410f3f746b5115df51df873f56b9cd2
Eric Levine <djlevine@gmail.com>
2013-09-25 19:46:20 -0700

    implemented facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,42 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable,
+  devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :trackable, :validatable
 
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid
+  attr_accessible :email, :firstname, :lastname, :password, :password_confirmation, :remember_me, :provider, :uid, :name
   has_and_belongs_to_many :wagers
 
-  attr_accessible :email, :firstname, :lastname
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+  user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20]
+                           )
+    end
+  user
+  end
+
+  # Notice that Devise RegistrationsController by default calls "User.new_with_session" 
+  # before building a resource. This means that, if we need to copy data from session whenever 
+  # a user is initialized before sign up, we just need to implement new_with_session in our model. 
+  # Here is an example that copies the facebook email if available:
+  
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  # @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,12 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  #OmniAuth - Facebook Configuration
+  require "omniauth-facebook"
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"]
+      {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
+
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.

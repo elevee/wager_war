3d682bd3ed82e4817b310587073d982a16f687ad
Eric Levine <djlevine@gmail.com>
2013-10-16 22:46:34 -0700

    organized crude terms layout

--- app/models/wager.rb
+++ app/models/wager.rb
@@ -3,7 +3,7 @@ class Wager < ActiveRecord::Base
   belongs_to :host,  class_name: "User", foreign_key: "host_id"
   belongs_to :guest,  class_name: "User", foreign_key: "guest_id"  
   has_many :terms, :dependent => :destroy
-  accepts_nested_attributes_for :terms, allow_destroy: true, :reject_if => lambda { |a| a[:terms].blank?  }
+  accepts_nested_attributes_for :terms, allow_destroy: true , :reject_if => lambda { |a| a[:terms].blank?  }
 
   attr_accessible :title, :description, :terms, :terms_attributes
 

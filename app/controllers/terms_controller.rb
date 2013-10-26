cb770d912b811956e9e0350a805f9dd029951f1a
Eric Levine <djlevine@gmail.com>
2013-10-25 18:07:07 -0700

    Added title markup to show page, commented out terms on new page for now

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,15 +1,30 @@
 class TermsController < ApplicationController
 
+  before_filter :get_wager
+
   def index
     
   end
 
   def create
-    
+    @term = @wager.terms.build(params[:terms])
+    respond_to do |format|
+      if @term.save
+        format.html { redirect_to wager_url(@wager), notice: 'Term was successfully created.'}
+        format.json { render json: @term, status: :created, location: @term }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @term.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def new
-    @term = Term.new
+    @term = @wager.terms.build
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @term }
   end
 
   def update
@@ -17,5 +32,12 @@ class TermsController < ApplicationController
 
   def destroy
   end
+
+  private
+  
+    # get_wager gets wager_id and turns it into a @term object  
+    def get_wager
+      @wager = Wager.find(params[:id])
+    end
 
 end
--- app/controllers/wagers_controller.rb
+++ app/controllers/wagers_controller.rb
@@ -1,45 +1,52 @@
 class WagersController < ApplicationController
 
-before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def index
     
   end
 
   def create
-    # Assign current user and guest user to wager and create it so it has an ID
+    # Create new wager from params
     @wager = Wager.new(params[:wager])
+      # Assign current user 
       @wager.host_id = current_user.id
-      # still need to assign guest user id
-    if @wager.save  
-      # Create terms
-      @term = []
-      @term.push(Term.create(params[:term]))
-      binding.pry
-      # Add terms to existing wager
-      @wager.terms.each do |term|
-        # @wager.host_terms.push(term.id)
-      end
-      redirect_to :action => 'show'
-    else
-      render :action => 'new'
-    end
+      # Assign guest user
+
+    @term = @wager.terms.build(params[:terms])
+
+    #   # Create terms
+    #   @host_terms = params[:wager][:terms_attributes]
+    #   @guest_terms = 
+
+    #   @term = []
+    #   @term.push(Term.create(params[:term]))
+    #   binding.pry
+    #   # Add terms to existing wager
+    #   @wager.terms.each do |term|
+    #     # @wager.host_terms.push(term.id)
+    #   end
+    #   redirect_to :action => 'show'
+    # else
+    #   render :action => 'new'
+    # end
 
     # params[:term].each
     # @term = @wager.terms.build(params[:term])
     # @term.save
 
-      if @wager.valid? 
-        respond_to do |format|
-          if @wager.save
-            format.html { redirect_to(@wager, :notice => 'Wager Stub successfully created.') }
-            format.xml  { render :xml => @wager, :status => :created, :location => @wager }
-          else
-            format.html { render :action => "new" }
-            format.xml  { render :xml => @wager.errors, :status => :unprocessable_entity }
-          end
+    if @wager.valid? 
+      respond_to do |format|
+        if @wager.save
+          binding.pry
+          format.html { redirect_to(@wager, :notice => 'Wager Stub successfully created.') }
+          format.xml  { render :xml => @wager, :status => :created, :location => @wager }
+        else
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @wager.errors, :status => :unprocessable_entity }
         end
       end
+    end
   end
 
   def new
